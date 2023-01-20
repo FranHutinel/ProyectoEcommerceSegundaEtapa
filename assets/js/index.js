e1f0fc00e2d4cf112678874d511bda5d6a2f61fa
Francisca Hutinel <M.ohutinel@gmail.com>
2023-01-20 15:56:31 -0300

    se realiza modal en template

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,19 +19,62 @@ function cargarProductos(listadoProductos) {
       let template = `
               <div class="col-12 col-md-6 col-lg-4">
                   <div class="card m-auto my-3" style="width: 18rem;">
-                      <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre
-        }">
+                      <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
                       <div class="card-body">
                       <h5 class="card-title">${producto.nombre}</h5>
                       <p class="card-text">${producto.sku}</p>
                       <p class="card-text">Precio Normal: $ ${producto.precio}</p>
-                      <p class="card-text text-danger">Descuento: -  $ ${producto.descuento
-        }</p>
-                      <p class="card-text text-success">Precio final: $ ${producto.precio - producto.descuento
-        }</p>
-                      <a class="btn btn-primary" data-sku="${producto.sku
-        }" onclick="addToCart('${producto.sku}')">Comprar</a>
+                      <p class="card-text text-danger">Descuento: -  $ ${producto.descuento}</p>
+                      <p class="card-text text-success">Precio final: $ ${producto.precio - producto.descuento}</p>
+                      <a class="btn btn-primary" data-sku="${producto.sku}" onclick="addToCart('${producto.sku}')">Comprar</a>
+
+
+      
+
+                       <button type="button" class="mt-0 mx-0 btn btn-primary"
+                       data-bs-toggle="modal" data-bs-target="#detallesmodal">Detalles</button>
+                        
+
+                       
+                       <div class="modal" tabindex="-1" id="detallesmodal">
+                       <div class="modal-dialog">
+                       <div class="modal-content">
+
+
+                       <div class="modal-header">
+
+                       <h5 class="modal-title">${producto.nombre}</h5>
+                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+                       </div>
+
+                       <div class="modal-body">
+                      
+                       <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre
+                       }"> 
+                      
+                      <p class="card-text">${producto.sku}</p>
+                      <p class="card-text">Precio Normal: $ ${producto.precio}</p>
+                      <p class="card-text">${producto.descripcion}</p>
+                      <p class="card-text text-danger">Descuento: -  $ ${producto.descuento}</p>
+                      <p class="card-text text-success">Precio final: $ ${producto.precio - producto.descuento}</p>
+
+                       </div>
+
+
+                       <div class="modal-footer">
+                       
+                       <a class="btn btn-primary" data-sku="${producto.sku
+                       }" onclick="addToCart('${producto.sku}')">Comprar</a>
+
+                       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
                       </div>
+                      </div>
+                      </div>
+                      </div>
+       
+                       
+                     </div>
                   </div>
               </div>
           `;
--- assets/js/productos.js
+++ assets/js/productos.js
@@ -1,75 +1,10 @@
-
-
-
-// let productos = [
-//     {
-//       id: "p0001",
-//       nombre: "Fifa 23 Ps4",
-//       descripcion: "Juego Fifa 23 para PS4.",
-//       precio: 49990,
-//       stock: 100,
-//       imagen: "../img/fifa23.jpg"
-//     },
-//     {
-//       id: "p0002",
-//       nombre: "God of war",
-//       descripcion: "Juego God of war RAGNAROK para PS4.",
-//       precio: 64990,
-//       stock: 100,
-//       imagen: "../assets/img/godofwar.jpg"
-//     },
-//     {
-   
-//       id: "p0003",
-//       nombre: "Pac man",
-//       descripcion: "Juego Pac-man and the ghostly adventures 2 para XBOX 360.",
-//       precio: 29990,
-//       stock: 100,
-//       imagen: "../assets/img/pacman.jpeg"
-//     },
-//     {
-    
-//       id: "p0004",
-//       nombre: "Mario rabbids",
-//       descripcion: "Juego Mario rabbids para PS4.",
-//       precio: 29990,
-//       stock: 100,
-//       imagen: "../img/mariorabbids.jpg"
-//     },
-//     {
-//       id: "p0005",
-//       nombre: "Sin city",
-//       descripcion: "Juego Sin city para PC.",
-//       precio: 34990,
-//       stock: 100,
-//       imagen: "../img/cimcity.jpeg"
-//     },
-//     {
-     
-//       id: "p0006",
-//       nombre: "World of warcraft",
-//       descripcion: "Juego World of warcraft para PC",
-//       precio: 21990,
-//       stock: 100,
-//       imagen: "../img/warcraft.jpeg"
-//     },
-  
-//   ];
-
-//   export default productos;
-
-
-
-
-
-
-
-  let productos = [
+let productos = [
     {
       sku: "0001",
       nombre: "Pokemon Esmeralda",
       precio: 49990,
       descuento: 1000,
+      descripcion: "Juego Pokemon versión esmeralda para GameBoy",
       imagen: "../assets/img/pokemonesmeralda.jpg"
     },
     {
@@ -77,6 +12,7 @@
       nombre: "Pokemon Amarillo",
       precio: 64990,
       descuento: 1000,
+      descripcion: "Juego Pokemon versión amarillo para GameBoy",
       imagen: "../assets/img/pokemonamarillo.jpg"
     },
     {
@@ -84,6 +20,7 @@
       nombre: "Pokemon Scarlet",
       precio: 29990,
       descuento: 1000,
+      descripcion: "Juego Pokemon versión scarlet para Switch",
       imagen: "../assets/img/pokemonscarlet.jpg"
     },
     {
@@ -91,6 +28,7 @@
       nombre: "Pokemon Violet",
       precio: 29990,
       descuento: 1000,
+      descripcion: "Juego Pokemon versión Violet para Switch",
       imagen: "../assets/img/pokemonviolet.jpg"
     },
     {
@@ -98,6 +36,7 @@
       nombre: "Pokemon Rojo",
       precio: 34990,
       descuento: 1000,
+      descripcion: "Juego Pokemon versión Rojo para Gameboy",
       imagen: "../assets/img/pokemonrojo.jpg"
     },
     {
@@ -105,6 +44,7 @@
       nombre: "Pokemon Azul",
       precio: 21990,
       descuento: 1000,
+      descripcion: "Juego Pokemon versión Azul para GameBoy",
       imagen: "../assets/img/pokemonazul.jpg"
     },
   
