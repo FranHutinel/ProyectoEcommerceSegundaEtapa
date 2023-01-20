15f168d42ab97d0563081409ce01d36d89a25dc8
Francisca Hutinel <M.ohutinel@gmail.com>
2023-01-19 22:03:46 -0300

    cambios en carrito, se aplica correctamente la logica de descuentos y sumar y restar cantidad de productos se deja operativo

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,12 +1,19 @@
 let cupones = [
     {
       nombre: "10%",
-      descuento: 10
+      descuento: 10,
+      estado: true,
     },
     {
       nombre: "20%",
-      descuento: 20
-    }
+      descuento: 20,
+      estado: true
+    },
+    {
+      nombre: "50%",
+      descuento: 50,
+      estado: true
+    },
   ];
   
   let productosCarro = [];
@@ -53,20 +60,26 @@ let cupones = [
                   <td>${productoConDetalles.precio}</td>
                   <td>${productoConDetalles.descuento}</td>
                   <td>${precioUnitario}</td>
-                  <td>${producto.cantidad}</td>
-                  <td>${totalProducto}</td>
+                  <td>
+                  <button onclick="restar('${productoConDetalles.sku}')">-</button>
+                  <input type="number" value="${
+                    producto.cantidad
+                  }" style="width:30px;" min="0" max="10">
+                  <button onclick="sumar('${productoConDetalles.sku}')">+</button>
+                </td>
+                <td>${totalProducto}</td>
               </tr>
       `;
       acumuladorFilas += template;
     });
   
-    document.querySelector("tbody").innerHTML =
+    document.querySelector("#productos-carrito tbody").innerHTML =
       acumuladorFilas;
       document.querySelector("#precio-total").innerHTML = `El precio total de la compra es: <strong>$${precioTotalCompra}</strong>`
   }
   
   function encontrarProducto(sku){
-    let encontrado = productos.find(producto => producto.sku == sku)
+    let encontrado = productos.find((producto) => producto.sku == sku)
     return encontrado;
   }
   
@@ -82,17 +95,57 @@ let cupones = [
     
    let cuponIngresado = document.getElementById("input-cupon").value;
   
-   let cuponEncontrado = cupones.find(cupon => cupon.nombre == cuponIngresado );
+   let cuponEncontrado = cupones.find((cupon) => cupon.nombre == cuponIngresado );
   
-   if(cuponEncontrado){
-    alert("cupón encontrado.")
-    precioTotalCompra = precioTotalCompra - (precioTotalCompra * cuponEncontrado.descuento/100)
-    document.querySelector("#precio-total").innerHTML = `El precio total de la compra con descuento es: <strong>$${precioTotalCompra}</strong>`
-   }else{
-    alert("El cupón no existe.")
-   }
-  
-   
-  
-  
-  })
+   if (cuponEncontrado && cuponEncontrado.estado == true) {
+    alert("cupón encontrado.");
+    precioTotalCompra =
+      precioTotalCompra -
+      (precioTotalCompra * cuponEncontrado.descuento) / 100;
+    document.querySelector(
+      "#precio-total"
+    ).innerHTML = `El precio total de la compra con descuento es: <strong>$${precioTotalCompra}</strong>`;
+    cuponEncontrado.estado = false;
+  } else {
+    alert("El cupón no existe. / o está caducado");
+  }
+});
+
+
+  //SUMAR PRODUCTOS
+
+  function restar(sku){
+
+    productosCarro.forEach((producto, index) => {
+      if(sku == producto.sku){
+        producto.cantidad = producto.cantidad - 1;
+        if(producto.cantidad <= 0){
+          if(confirm("Está seguro que desea eliminar?")){
+            productosCarro.splice(index, 1)
+          }else{
+            producto.cantidad =1;
+          }
+        }
+      }
+    })
+    actualizarCarro(productosCarro);
+    cargarTablaProductos();
+  }
+
+
+  //RESTAR PRODUCTOS
+
+  function sumar(sku){
+
+    productosCarro.forEach((producto, index) => {
+      if(sku == producto.sku){
+        producto.cantidad = producto.cantidad + 1;
+        if(producto.cantidad >= 10){
+          producto.cantidad =10;
+          alert("Alcanzo el limite de productos permitidos (10 unidades)")
+        }
+      }
+    })
+    actualizarCarro(productosCarro);
+    cargarTablaProductos();
+  }
